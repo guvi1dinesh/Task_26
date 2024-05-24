06bb2057503ded9400601cc5ef405168edee048a
guvi1dinesh <149289110+guvi1dinesh@users.noreply.github.com>
2024-05-24 09:09:19 -0400

    Add files via upload

---
+++ test_data.py
@@ -0,0 +1,27 @@
+# Python Class for Username and Password
+class TestData:
+    name = "Rahman"
+    birthdate = "02-01-1967"
+    birthdate1 = "07-01-2024"
+
+
+# Python Class for Selenium Selectors
+class TestSelectors:
+    expand_all_xpath = ("/html/body/div[2]/main/div[2]/div[4]/section/section/div/section/section/div["
+                        "2]/div/section/div[2]/div[1]/div/button/span")
+
+    input_name = "//*[@data-testid='test-nametext']"
+
+    input_birthdate = '//*[@data-testid="birthDate-start"]'
+
+    input_birthdate1 = '//*[@data-testid="birthDate-end"]'
+
+    awards_xpath = ("/html/body/div[2]/main/div[2]/div[4]/section/section/div/section/section/div[2]/div/section/div["
+                    "2]/div[1]/section/div/div[4]/div[2]/div/section/button[14]/span")
+
+    gender_male_xpath = ("/html/body/div[2]/main/div[2]/div[4]/section/section/div/section/section/div["
+                         "2]/div/section/div[2]/div[1]/section/div/div[7]/div[2]/div/section/button[1]/span")
+
+    see_result_xpath = ("/html/body/div[2]/main/div[2]/div[4]/section/section/div/section/section/div["
+                        "2]/div/section/div[1]/button")
+
